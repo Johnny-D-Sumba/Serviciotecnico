f2294472b7307917fdf55a202609e1c1bdf08b8f
Johnny D. Sumba <jhonnydanilo177@gmail.com>
2022-08-23 17:35:12 -0500

    Empresa service updated

---
+++ src/service/EmpresaService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export class EmpresaService {
+    getEmpresas() {
+        return axios.get('http://localhost:9090/api/v1.0/empresa')
+            .then(res => res.data.result);
+    }
+
+    postEmpresas(emp) {
+        return axios.post('http://localhost:9090/api/v1.0/empresa', emp)
+    }
+}
